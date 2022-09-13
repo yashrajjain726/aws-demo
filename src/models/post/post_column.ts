062c78359bfaad7b1ab7452b782880d9b0a19a72
Yashraj Jain <yashrajjain726@gmail.com>
2022-09-13 16:54:58 +0530

    models created for post

---
+++ src/models/post/post_column.ts
@@ -0,0 +1,8 @@
+class PostColumn {
+  public static readonly ID = "id";
+  public static readonly NAME = "name";
+  public static readonly EMAIL = "email";
+  public static readonly PHONENO = "phone_no";
+  public static readonly USERTYPE = "user_type";
+}
+export default PostColumn;
